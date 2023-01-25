52f73204761d76db148c8d380674ae066731b40e
zoom Дмитрий <z-oo-m@yandex.ru>
2023-01-25 22:27:26 +0100

    Программная реализация преобразования массива из элементов текста  <= 3

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿void Find(string[] text){
+
+text = Array.FindAll(text, i => i.Length <= 3).ToArray();
+        Console.WriteLine(String.Join(",", text));
+}
+
+Console.Clear();
+Console.Write("Введите элементы массива через пробел: ");
+string[] arrayText = Console.ReadLine().Split(" ").ToArray();
+Find(arrayText);
